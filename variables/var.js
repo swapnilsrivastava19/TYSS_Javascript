ca18e7a9e6c43ecf36bb79f25db9a37ddd6bae43
swapnil <swapnilsrivastava92.ss@gmail.com>
2023-03-15 17:35:25 +0530

    Javascript

---
+++ variables/var.js
@@ -0,0 +1,22 @@
+//var a=10
+//var a=20
+//var a='javascript'
+
+//let a=10
+//a=20
+
+//const a=30
+//a='java'
+//const a='Hellonod'
+
+let a=10
+
+console.log(typeof(String(a)));
+
+var b=20
+
+console.log(typeof (b))
+
+const c=30
+
+console.log(typeof(String(c)))
